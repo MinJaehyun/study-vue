856ad9317c0d33b2c00ef7204677618bf73491a4
DESKTOP-QPGUH2T\sv <jhmin@sharevaluecorp.com>
2022-05-19 18:24:07 +0900

    불 필요한 코드 제거

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <template>
-  <!-- 아래 그냥 컴포넌트를 불러오는 것과 router-view 를 통해 URL 접근 시 불러오는 것의 차이점을 이해 -->
-  <!--  <TodoList />-->
   <router-view></router-view>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-// import TodoList from '@/components/TodoList.vue';
 
 export default defineComponent({
   name: 'App',
-  // components: {
-  //   TodoList,
-  // },
 });
 </script>
 <style>
