ee284551981fef45ddfa1571d2ff468e04695912
DESKTOP-QPGUH2T\sv <jhmin@sharevaluecorp.com>
2022-05-19 18:24:48 +0900

    Add TodoInput & ts

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,13 +1,37 @@
 <template>
-$END$
+  <div>
+    <label for="todo-input"></label>
+    <input @input="inputHandle" id="todo-input" type="text" :value="item" />
+    <button @click="addTodo" type="button">추가</button>
+  </div>
 </template>
 
-<script>
-export default {
-name: "TodoInput"
-}
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'TodoInput',
+  props: ['item'],
+  data() {
+    return {
+      item2: 'abc',
+    };
+  },
+  methods: {
+    addTodo() {
+      // console.log('test');
+      this.$emit('addToStorage');
+    },
+    // event 는 타입이 InputEvent 이다.
+    inputHandle(event: InputEvent) {
+      // console.log('e', e);
+      // event.target 는 html element 이므로 아래처럼 설정하면 된다.
+      // event.target 는 HTMLInputElement 이라는 설정
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit('inputValue', eventTarget.value);
+    },
+  },
+});
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
