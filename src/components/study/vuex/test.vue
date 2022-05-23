93c13223fd005b40200060a418739150f3dd67bb
DESKTOP-QPGUH2T\sv <jhmin@sharevaluecorp.com>
2022-05-23 18:57:47 +0900

    vuex setup

--- src/components/study/vuex/test.vue
+++ src/components/study/vuex/test.vue
@@ -1,13 +1,21 @@
 <template>
-$END$
+  <div>{{ $store.state.counter }}</div>
+  <button @click="setAsyncCounter">동기적 카운터 증가</button>
+  <button @click="setSyncCounter">비동기적 카운터 증가</button>
 </template>
 
 <script>
 export default {
-name: "test"
-}
+  name: 'test-vuex',
+  methods: {
+    setAsyncCounter() {
+      this.$store.commit('setAsyncCounter');
+    },
+    setSyncCounter() {
+      this.$store.dispatch('setSyncCounter');
+    },
+  },
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
