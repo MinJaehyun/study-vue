6067dee5d66fece6ef2239cc158cb39f3f53bf92
{krism01} <{krism01@naver.com}>
2022-07-23 15:32:11 +0900

    fix eslint, prettier 재설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,42 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/prettier',
-    // "eslint:recommended",
-    '@vue/typescript/recommended',
-    // 'plugin:prettier/recommended',
-  ],
-  rules: {
-    // "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    // "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    'no-console': 'off',
-    'vue/multi-word-component-names': [
-      'error',
-      {
-        ignores: ['default', 'Todo'],
-      },
-    ],
-    // 'prettier/prettier': [
-    //   'error',
-    //   {
-    //     printWidth: 80,
-    //     singleQuote: true,
-    //     semi: true,
-    //     tabWidth: 2,
-    //     trailingComma: 'all',
-    //     bracketSpacing: true,
-    //     arrowParens: 'avoid',
-    //     endOfLine: 'auto',
-    //   },
-    // ],
-  },
-  parserOptions: {
-    ecmaVersion: 2020,
-  },
+	root: true,
+	env: {
+		node: true,
+	},
+	extends: [
+		'plugin:vue/vue3-essential',
+		'@vue/prettier',
+		'@vue/typescript/recommended',
+		// "eslint:recommended",
+		// 'plugin:prettier/recommended',
+	],
+	rules: {
+		// "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+		// "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+		'no-console': 'off',
+		'vue/multi-word-component-names': [
+			'error',
+			{
+				ignores: ['default', 'Todo'],
+			},
+		],
+		'prettier/prettier': [
+			'error',
+			{
+				printWidth: 80,
+				endOfLine: auto,
+				singleQuote: true,
+				semi: true,
+				useTabs: true,
+				tabWidth: 2,
+				trailingComma: 'all',
+				bracketSpacing: true,
+				arrowParens: 'avoid',
+			},
+		],
+	},
+	parserOptions: {
+		ecmaVersion: 2020,
+	},
+	ignorePatterns: settings.json,
 };
