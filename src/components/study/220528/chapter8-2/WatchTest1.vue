99d5cf64306394fe3ca2693859c81268a0e39575
{krism01} <{krism01@naver.com}>
2022-08-24 11:59:20 +0900

    watch

--- src/components/study/220528/chapter8-2/WatchTest1.vue
+++ src/components/study/220528/chapter8-2/WatchTest1.vue
@@ -39,3 +39,12 @@ export default defineComponent({
   1. === -1 이면 존재하지 않는 경우 (-1)
   2. > -1 이면 존재하는 경우 (0 또는 양수)
 -->
+
+<!-- 흐름
+  1. 목표 적기
+  2. 내용을 입력하기 위한 textarea 설정
+  3. 입력한 내용을 v-model 설정하여 양방향 데이터 바인딩
+  4. 입력한 문자열을 watch 기능으로 감시하여 금지 문자열이 입력하면 alert 띄우기
+    - 금지문자열 forbiddenText data 설정하기
+    - inputText 안에 forbiddenText 가 존재하는지 함수 만들고, 있으면 alert 띄우기
+ -->
